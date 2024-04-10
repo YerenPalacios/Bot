88e60df9f8352e506b119193e3f97fb3782c86ba
Yeren Palacios <yerenagmt@gmail.com>
2024-04-10 16:23:49 -0500

    ...

--- scrapers/campus.py
+++ scrapers/campus.py
@@ -15,9 +15,9 @@ class Campus:
 
     def __init__(self):
         chrome_options = Options()
-        # chrome_options.add_argument("--no-sandbox")
-        # chrome_options.add_argument("--headless")
-        # chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--disable-dev-shm-usage")
         
 
         try:
