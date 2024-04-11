b211b3e4324c8f6ffcaea9f0fc55d077f8c8abd9
Yeren Palacios <yerenagmt@gmail.com>
2024-04-11 00:11:22 -0500

    send campus messages

--- scrapers/campus.py
+++ scrapers/campus.py
@@ -6,7 +6,8 @@ from selenium.common import WebDriverException, SessionNotCreatedException
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
-# from constants import COOKIES
+from constants import COOKIES
+
 
 logger = logging.getLogger(__name__)
 
@@ -21,12 +22,7 @@ class Campus:
         
 
         try:
-            options = webdriver.ChromeOptions()
-            options.add_experimental_option("detach", True)
-            self.driver = webdriver.Remote(
-                command_executor="http://172.17.0.1:4444/wd/hub",
-                options=options
-            )
+            self.driver = webdriver.Chrome()
             self.driver.delete_all_cookies()
             logger.info("Chrome driver started")
             self.driver.get("https://campus0d.unad.edu.co/campus/miscursos.php")
@@ -37,6 +33,11 @@ class Campus:
             raise e
 
     def login(self):
+        time.sleep(1)
+        failed_div = self.find(By.CLASS_NAME, "Cuerpo600")
+        if failed_div and failed_div[0].text == 'Acceder a Campus Virtual\nSu navegador no logro procesar las cookies para el sitio unad.edu.co':
+            self.driver.delete_all_cookies()
+            self.driver.get("https://campus0d.unad.edu.co/campus/miscursos.php")
         user_field = self.driver.find_element(By.ID, 'txtuser')
         user_field.send_keys('1022322066')
         button = self.driver.find_element(By.ID, 'cmdIngresa2')
@@ -48,8 +49,8 @@ class Campus:
         button = self.driver.find_element(By.ID, 'cmdIngresa2')
         button.click()
         print('Login succesful')
-
-        # COOKIES.update(self.driver.get_cookies())
+        global COOKIES 
+        COOKIES += self.driver.get_cookies()
 
         self.driver.get('https://campus0d.unad.edu.co/campus/miscursos.php')
 
@@ -87,4 +88,68 @@ class Campus:
             self.driver.close()
 
         return unread_posts_text
+    
+    def read_courses_email(self):
+        self.driver.get('https://campus0d.unad.edu.co/campus/miscursos.php')
+        time.sleep(1)
+        cards = self.driver.find_elements(By.CLASS_NAME, "card-curso")
+
+        course_message = {}
+        for i in range(len(cards)):
+            time.sleep(1)
+            course_card = self.driver.find_elements(By.CLASS_NAME, "card-curso")[i]
+            button = course_card.find_element(By.TAG_NAME, "button")
+            course_id =course_card.find_element(By.TAG_NAME, "b").text
+            button.click()
+            self.driver.switch_to.window(self.driver.window_handles[-1])
+            time.sleep(2)
+            messages = self.read_email()
+            course_message[course_id] = messages
+            self.driver.get('https://campus0d.unad.edu.co/campus/miscursos.php')
+
+        return course_message
+    
+    def read_email(self):
+        email_button = self.driver.find_element(By.ID, "nav-mail-popover-container")
+        email_button.click()
+
+        mail_elements = self.driver.find_element(By.CLASS_NAME, "mail-navbar-menu").find_elements(By.TAG_NAME, 'a')
+        bandeja = mail_elements[0]
+        # ???
+        unread = self.find(By.TAG_NAME, 'span', bandeja)
+        if not unread:
+            return []
+        
+        unread_amount = unread[0].text
+
+        if not bool(unread_amount):
+            return []
+        
+        bandeja.click()
+        messages = []
+        unread_mesage_divs = self.find(By.CLASS_NAME, 'mail_unread', 0)
+        for _ in range(len(unread_mesage_divs)):
+            time.sleep(1)
+            self.find(By.CLASS_NAME, 'mail_unread', 0)[0].click()
+            time.sleep(2)
+            messages.append("--------\n\n"+self.find(By.CLASS_NAME, 'mail_content')[0].text)
+            self.find(By.CLASS_NAME, 'mail_goback')[0].click()
+            time.sleep(1)
+
+        return messages
+
+
+
+    def find(self, by: str, value: str, of: any = 0):
+        if of:
+            try:
+                return of.find_elements(by, value)
+            except:
+                return
+        try:
+            return self.driver.find_elements(by, value)
+        except:
+            return
+            
+
     
