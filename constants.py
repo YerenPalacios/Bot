c9239dcf0ee8df8167df80b97565920d34a59dc7
Yeren Palacios <yerenagmt@gmail.com>
2024-04-20 09:56:59 -0500

    Fix to get courses card from first group of cards and save cookies in a file

--- constants.py
+++ constants.py
@@ -1,3 +1,4 @@
+import json
 import os
 
 BOT_API_KEY = os.getenv('BOT_API_KEY')
@@ -7,3 +8,5 @@ TELEGRAM_API_URL = f"https://api.telegram.org/bot{BOT_API_KEY}"
 #campus
 C_ID_USUARIO="1094047"
 COOKIES = []
+with open('cookies.txt', 'rb') as f:
+    COOKIES.extend(json.loads(f.read()))
