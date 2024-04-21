3d7c055a3520bb2185ba2015b437e5ffad90e4f9
Yeren Palacios <yerenagmt@gmail.com>
2024-04-21 02:55:47 -0500

    remove cookies.txt file

--- constants.py
+++ constants.py
@@ -8,5 +8,8 @@ TELEGRAM_API_URL = f"https://api.telegram.org/bot{BOT_API_KEY}"
 #campus
 C_ID_USUARIO="1094047"
 COOKIES = []
-with open('cookies.txt', 'rb') as f:
-    COOKIES.extend(json.loads(f.read()))
+try:
+    with open('cookies.txt', 'rb') as f:
+        COOKIES.extend(json.loads(f.read()))
+except FileNotFoundError:
+    ...
