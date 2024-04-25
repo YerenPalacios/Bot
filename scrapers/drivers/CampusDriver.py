903472944d2d741d66421c2b062f38c1da997b20
Yeren Palacios <yerenagmt@gmail.com>
2024-04-25 09:31:55 -0500

    add chrome options

--- scrapers/drivers/CampusDriver.py
+++ scrapers/drivers/CampusDriver.py
@@ -20,7 +20,9 @@ class CampusDriver:
 
     def __init__(self):
         chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--disable-dev-shm-usage")
         # Unknown options
         # chrome_options.add_argument("--enable-features=AllowGeolocationOnInsecureOrigins")
         # chrome_options.add_argument("--disable-features=BlockNonSecureSubresources")
